4e5cb5df0ae949811b3726773a30b8b206e2ac91
michaelsong93 <ss7277@seas.upenn.edu>
2021-08-30 19:52:45 +0900

    1

--- aug30-1.py
+++ aug30-1.py
@@ -1,1 +1,7 @@
-print(1)
+# name = input('what is your name? ')
+# color = input("what is your favorite color? ")
+# print(name + " likes " + color)
+
+birth = input("birth year: ")
+age = 2021-int(birth)
+print(age)
