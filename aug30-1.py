7ecce63274604c6c420cc3392ec75c295ba7dbb6
michaelsong93 <ss7277@seas.upenn.edu>
2021-09-02 18:47:51 +0900

    2

--- aug30-1.py
+++ aug30-1.py
@@ -2,6 +2,30 @@
 # color = input("what is your favorite color? ")
 # print(name + " likes " + color)
 
-birth = input("birth year: ")
-age = 2021-int(birth)
-print(age)
+# birth = input("birth year: ")
+# print(type(birth))
+# age = 2021-int(birth)
+# print(type(age))
+# print(age)
+
+# weight = input("weight: ")
+# weight_kg = float(weight) * 0.45
+# print(weight_kg)
+
+# course = "python's course for beginners"
+# course1 = 'python course for "beginners"'
+# course2 = '''hello
+# hell0 1 
+# 12345'''
+# course3 = 'python'
+# print(course3[0])
+# print(course3[-1])
+# print(course3[-2])
+# print(course3[0:])
+# print(course3[0:-1])
+# # print(course3[:7])
+# print(course3[:])
+
+# another = course3[:]
+# print(another)
+
