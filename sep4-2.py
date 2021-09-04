4a7fc8628a2303a936a2ee3b1e636b889f46b6bc
michaelsong93 <ss7277@seas.upenn.edu>
2021-09-04 18:22:46 +0900

    5

--- sep4-2.py
+++ sep4-2.py
@@ -13,3 +13,21 @@ def f(n):
     yield n*n
 
 print([i for i in f(3)])
+
+def merge(l,r):
+    llen = len(l)
+    rlen = len(r)
+    i = 0
+    j = 0
+    while i < llen or j < rlen:
+        if j == rlen or (i < llen and l[i] < r[j]):
+            yield l[i]
+            i += 1
+        else:
+            yield r[j]
+            j += 1
+# g = merge([1,3,5],[2,4,6])
+# while True:
+#     print(g.__next__())
+# print(merge([1,3,5],[2,4,6]))
+print([x for x in merge([1,2,5],[3,4,6])])
